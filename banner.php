04798c31e7c3716dcdf5ed5948e87c7e96a5c2c2
Francis Zhou <hzhou14@u.rochester.edu>
2014-10-19 16:31:11 -0400

    Create banner.php

---
+++ banner.php
@@ -0,0 +1,4 @@
+<!-- Start Banner html for inserted into all pages -->
+<img src="CampusFly.png" alt="CampusFly Logo" title="CampusFly Logo">
+<p id="bannerP">CampusFly</p>
+<!-- End Banner html -->
