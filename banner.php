3808f628921153f14b77452afb21b64b85e5becb
Rikesh Thapa <rthapa@u.rochester.edu>
2015-02-05 21:19:08 -0500

    Delete banner.php
    Delete this we will not be using this form

--- banner.php
+++
@@ -1,4 +0,0 @@
-<!-- Start Banner html for inserted into all pages -->
-<img src="CampusFly.png" alt="CampusFly Logo" title="CampusFly Logo">
-<p id="bannerP">CampusFly</p>
-<!-- End Banner html -->
